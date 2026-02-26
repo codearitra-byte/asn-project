9528dbef60d519948c97e0782c1f93a97e46dd27
Aritra Nandy <aritranandy123@gmail.com>
2026-02-26 19:45:13 +0530

    Initial commit

--- dags/asn_pipeline_dag.py
+++ dags/asn_pipeline_dag.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 
 from enrich.peering_db import enrich_asn
 from extract.fetch_delegated import fetch_asn
-from load.load_postgres import load_asn
+# from load.load_postgres import load_asn
 
 with DAG(
     dag_id="asn_pipeline",
@@ -20,12 +20,12 @@ with DAG(
         retry_delay=timedelta(minutes=1),
     )
 
-    load_task = PythonOperator(
-        task_id="load_to_postgres",
-        python_callable=load_asn,
-        retries=0,
-        retry_delay=timedelta(minutes=1),
-    )
+    # load_task = PythonOperator(
+    #     task_id="load_to_postgres",
+    #     python_callable=load_asn,
+    #     retries=0,
+    #     retry_delay=timedelta(minutes=1),
+    # )
     enrich_task = PythonOperator(
     task_id="enrich_asn",
     python_callable=enrich_asn,
